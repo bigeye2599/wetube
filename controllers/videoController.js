ec6166e6cfa82be7113c3461773049c03441ba87
bigeye2599 <bigeye2599@gmail.com>
2020-03-24 15:41:09 +0900

    #2.18 Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,10 +1,24 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) => res.send("Search", { pageTitle: "Search" });
-export const videos = (req, res) => res.send("Videos", { pageTitle: "Videos" });
-export const upload = (req, res) => res.send("Upload", { pageTitle: "Upload" });
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+
+  return res.render("search", { pageTitle: "Search", searchingBy });
+};
+
+export const videos = (req, res) =>
+  res.render("videos", { pageTitle: "Videos" });
+
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+
 export const videoDetail = (req, res) =>
-  res.send("videoDetail", { pageTitle: "Video Detail" });
+  res.render("videoDetail", { pageTitle: "videoDetail" });
+
 export const editVideo = (req, res) =>
-  res.send("editVideo", { pageTitle: "Edit Video" });
+  res.render("editVideo", { pageTitle: "editVideo" });
+
 export const deleteVideo = (req, res) =>
-  res.send("deleteVideo", { pageTitle: "Delete Video" });
+  res.render("deleteVideo", { pageTitle: "deleteVideo" });

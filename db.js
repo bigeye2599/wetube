6c3978f8315be95b4c716d23204dca431e76314b
bigeye2599 <bigeye2599@gmail.com>
2020-03-24 20:28:17 +0900

    #2.21 Home Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 111111,
+    title: "Video awesome 1",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "example",
+      email: "example@empty.com"
+    }
+  },
+  {
+    id: 222222,
+    title: "Video awesome 2",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "example",
+      email: "example@empty.com"
+    }
+  },
+  {
+    id: 333333,
+    title: "Video awesome 3",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "example",
+      email: "example@empty.com"
+    }
+  },
+  {
+    id: 444444,
+    title: "Video awesome 4",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "example",
+      email: "example@empty.com"
+    }
+  }
+];
